57439fb922100ae204ff2ea4ddfe993a9d3f9aa5
godhw <heewons.dev@gmail.com>
2023-05-23 11:24:08 +0900

    add base source files

---
+++ src/components/common/Icons/index.js
@@ -0,0 +1,61 @@
+import { ReactComponent as ChannelMeetingIcon } from '../../../icons/ic-channel-meeting.svg';
+import { ReactComponent as MeetingStopIcon } from '../../../icons/ic-meeting-stop.svg';
+import { ReactComponent as ScreenShareStartIcon } from '../../../icons/ic-screen-share-start.svg';
+import { ReactComponent as ScreenShareStopIcon } from '../../../icons/ic-screen-share-stop.svg';
+import { ReactComponent as CircleDeselectedIcon } from '../../../icons/ic-circle-deselected.svg';
+import { ReactComponent as CircleSelectedIcon } from '../../../icons/ic-circle-selected.svg';
+import { ReactComponent as GroupThumbnailUploadIcon } from '../../../icons/ic-group-thumbnail-upload.svg';
+import { ReactComponent as CaptureIcon } from '../../../icons/ic-capture.svg';
+import { ReactComponent as CameraOffIcon } from '../../../icons/ic-camera-off.svg';
+import { ReactComponent as CameraOnIcon } from '../../../icons/ic-camera-on.svg';
+import { ReactComponent as ModalCloseIcon } from '../../../icons/ic-modal-close.svg';
+import { ReactComponent as MicOffIcon } from '../../../icons/ic-mic-off.svg';
+import { ReactComponent as FileSelectIcon } from '../../../icons/ic-file-select.svg';
+import { ReactComponent as MicOnIcon } from '../../../icons/ic-mic-on.svg';
+import { ReactComponent as GroupAddIcon } from '../../../icons/ic-group-add.svg';
+import { ReactComponent as SpeakerOffIcon } from '../../../icons/ic-speaker-off.svg';
+import { ReactComponent as GroupDeleteIcon } from '../../../icons/ic-group-delete.svg';
+import { ReactComponent as SpeakerOnIcon } from '../../../icons/ic-speaker-on.svg';
+import { ReactComponent as ChannelAddIcon } from '../../../icons/ic-channel-add.svg';
+import { ReactComponent as GroupInviteIcon } from '../../../icons/ic-group-invite.svg';
+import { ReactComponent as ThreadCloseIcon } from '../../../icons/ic-thread-close.svg';
+import { ReactComponent as ChannelOpenIcon } from '../../../icons/ic-channel-open.svg';
+import { ReactComponent as ImageAddIcon } from '../../../icons/ic-image-add.svg';
+import { ReactComponent as ThreadOpenIcon } from '../../../icons/ic-thread-open.svg';
+import { ReactComponent as ChatOpenIcon } from '../../../icons/ic-chat-open.svg';
+import { ReactComponent as ImageUploadIcon } from '../../../icons/ic-image-upload.svg';
+import { ReactComponent as ChannelChattingIcon } from '../../../icons/ic-channel-chatting.svg';
+import { ReactComponent as ChatCloseIcon } from '../../../icons/ic-chat-close.svg';
+import { ReactComponent as LikeIcon } from '../../../icons/ic-like.svg';
+
+export {
+  CameraOffIcon,
+  CameraOnIcon,
+  CaptureIcon,
+  ModalCloseIcon,
+  MicOffIcon,
+  FileSelectIcon,
+  MicOnIcon,
+  GroupAddIcon,
+  SpeakerOffIcon,
+  GroupDeleteIcon,
+  SpeakerOnIcon,
+  ChannelAddIcon,
+  GroupInviteIcon,
+  ThreadCloseIcon,
+  ChannelOpenIcon,
+  ImageAddIcon,
+  ThreadOpenIcon,
+  ChatOpenIcon,
+  ImageUploadIcon,
+  ChannelChattingIcon,
+  ChatCloseIcon,
+  LikeIcon,
+  ChannelMeetingIcon,
+  MeetingStopIcon,
+  ScreenShareStartIcon,
+  ScreenShareStopIcon,
+  CircleDeselectedIcon,
+  CircleSelectedIcon,
+  GroupThumbnailUploadIcon,
+};
