bd7db4d20a65057f77a15432b068e059778bc342
손병재 <byeongjae.dev@gmail.com>
2023-05-29 21:45:16 +0900

    Fix: routing error

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import './App.css';
 import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/main" element={<Main />} />
-        <Route
-            path="/"
-            element={<SignIn />}
-          />
-          <Route
-            path="/signup"
-            element={(<SignUp />)}
-          />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={(<SignUp />)} />
+      </Routes>
     </div>
   );
 }
