de4a8aa7de27e8a5125fea59a4e419a22d21f58f
godhw <heewons.dev@gmail.com>
2023-06-05 01:35:04 +0900

    Add Empty component in main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,16 +4,19 @@ import ChannelHeader from '../../components/ChannelHeader';
 import Chat from '../../components/Chat';
 // import Meet from '@components/Meet';
 import SideBar from '../../components/SideBar';
-// import Empty from '@components/common/Empty';
+import Empty from '../../components/common/Empty';
 import { Layout, MainWrapper } from './style';
 
+const EMPTY_MESSAGE = '빈화면';
+
 function Main() {
+  const selectedChannel = 'empty';
   return (
     <Layout>
       <SideBar />
       <MainWrapper>
         <ChannelHeader />
-        <Chat />
+        {selectedChannel === 'empty' ? <Empty message={EMPTY_MESSAGE} /> : <Chat />}
       </MainWrapper>
     </Layout>
   );
